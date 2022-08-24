2f4aab90c0a3430cf1559b2b77f098e1c0652e15
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-08-24 15:15:38 +0200

    Login update

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-require ("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
 const jwt = require("jsonwebtoken");
 const db = require("./config/dbART");
-const { compare, hash} = require("bcrypt")
+const { compare, hash } = require("bcrypt");
 const app = express();
 const router = express.Router();
 
-const port = parseInt(process.env.PORT) || 4000
+const port = parseInt(process.env.PORT) || 4000;
 
+app.use(
+  cors({
+    origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
-    credentials: true
-  }))
+app.use(
+  express.static("public"),
+  router,
+  express.json(),
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-  app.use(
-    express.static("public"),
-    router,
-    express.json(),
-    express.urlencoded({
-      extended: true,
-    })
-  );
-  
-  //
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-  // home
-  router.get("/", (req, res) => {
-    res.sendFile(path.join (__dirname, "views", "index.html"));
-  });
+//
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+// home
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 // To get the users
-router.get("/register", (req,res) => {
+router.get("/register", (req, res) => {
   let strQry = `SELECT * FROM users;`;
-  db.query(strQry, (err, results)=>{
+  db.query(strQry, (err, results) => {
     if (err) throw err;
     res.status(200).json({
       results: results,
@@ -46,101 +47,145 @@ router.get("/register", (req,res) => {
   });
 });
 
-
-  // Register
+// Register
 router.post("/register", bodyParser.json(), (req, res) => {
+  let { user_name, user_lastname, email, user_password, user_role } = req.body;
 
-    let { user_name, user_lastname, email, user_password, user_role } = req.body;
+  if (user_role === null || user_role === undefined || user_role.length === 0) {
+    user_role = "user";
+  }
 
-    if ( (user_role === null) || (user_role === undefined) || (user_role.length === 0)) {
-      user_role = "user";
-    }
-
-    let strQry = `SELECT email, user_password
+  let strQry = `SELECT email, user_password
       FROM users
       WHERE LOWER(email) = LOWER('${email}')`;
-    db.query(strQry, async (err, results) => {
-      if(err){
-        throw err;
-      }else {
-        console.log(results);
-        if (results.length) {
-          res.status(409).json({ msg: "User already exist" });
-        } else {
-          // Encrypting a password
+  db.query(strQry, async (err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(results);
+      if (results.length) {
+        res.status(409).json({ msg: "User already exist" });
+      } else {
+        // Encrypting a password
 
-          user_password = await hash(user_password, 10);
+        user_password = await hash(user_password, 10);
 
-          strQry = `
+        strQry = `
                   INSERT INTO users(user_name, user_lastname , email, user_password, user_role)
                   VALUES(?, ?, ?, ?, ?);
                   `;
-          db.query(
-            strQry,
-            [user_name, user_lastname , email, user_password, user_role],
-            (err) => {
-              if (err) throw err;
-              res
-                .status(201)
-                .json({
-                  msg: `You have been successfully registered !!!`
-                });
-            }
-          );
-        }
-      } 
-    });
+        db.query(
+          strQry,
+          [user_name, user_lastname, email, user_password, user_role],
+          (err) => {
+            if (err) throw err;
+            res.status(201).json({
+              msg: `You have been successfully registered !!!`,
+            });
+          }
+        );
+      }
+    }
   });
+});
+// Login
 
-  // Adding Products
+router.post("/login", bodyParser.json(), (req, res) => {
+  const { email, user_password } = req.body;
 
-  router.post("/products", bodyParser.json(), (req, res) => {
-    const bd = req.body;
-    // bd.totalamount = bd.quantity * bd.price;
-    // Query
-    const strQry = `
+  const strQry = `
+    SELECT * FROM users
+    WHERE email = '${email}'
+    `;
+
+  db.query(strQry, async (err, results) => {
+    // console.log(results)
+    if (err) throw err;
+    //incorrect password
+    if (!results.length) {
+      res.status(404).json({ msg: "not registered" });
+    }
+    const ismatch = await compare(user_password, results[0].user_password);
+
+    if (ismatch === false) {
+      res.status(404).json({
+        msg: "Incorecct Password",
+      });
+    }
+    // Token
+    if (ismatch === true) {
+      const token = jwt.sign(
+        {
+          user_id: results[0].user_id,
+          user_name: results[0].user_name,
+          user_lastname: results[0].user_lastname,
+          email: results[0].email,
+        },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "1h",
+        },
+        (err, token) => {
+          if (err) throw err;
+          // Login
+          res.status(200).json({
+            msg: "Logged in",
+            token,
+            results: results[0],
+          });
+        }
+      );
+    }
+  });
+});
+// Adding Products
+
+router.post("/products", bodyParser.json(), (req, res) => {
+  const bd = req.body;
+  // bd.totalamount = bd.quantity * bd.price;
+  // Query
+  const strQry = `
       INSERT INTO products(title, category,  product_description, img, price, quantity)
       VALUES(?, ?, ?, ?, ?, ?);
       `;
-    //
-    db.query(
-      strQry,
-      [
-        bd.title,
-        bd.category,
-        bd.product_description,
-        bd.img,
-        bd.price,
-        bd.quantity,
-      ],
-      (err, results) => {
-        if (err) throw err;
-        res.json({
-          msg: "Product added successfully"
-        });
-      }
-    );
-  });
+  //
+  db.query(
+    strQry,
+    [
+      bd.title,
+      bd.category,
+      bd.product_description,
+      bd.img,
+      bd.price,
+      bd.quantity,
+    ],
+    (err, results) => {
+      if (err) throw err;
+      res.json({
+        msg: "Product added successfully",
+      });
+    }
+  );
+});
 
+// Call all Products
 
-  // Call all Products
-
-  router.get("/products", (req, res) => {
-    // Query
-    const strQry = `
+router.get("/products", (req, res) => {
+  // Query
+  const strQry = `
       SELECT *
       FROM products;
       `;
-    db.query(strQry, (err, results) => {
-      if (err) throw err;
-      res.json({
-        status: 200,
-        results: results,
-      });
+  db.query(strQry, (err, results) => {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      results: results,
     });
   });
+});
 
-  // Call single Project
+// Call single Project
 router.get("/products/:id", (req, res) => {
   // Query
   const strQry = `
