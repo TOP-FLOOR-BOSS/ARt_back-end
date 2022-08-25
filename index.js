bec7f573d2effd3263c226ef248d5105ba6f2a94
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-08-25 09:55:40 +0200

    Login update

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const { compare, hash } = require("bcrypt");
 const app = express();
 const router = express.Router();
 
-const port = parseInt(process.env.PORT) || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
+    origin: ["http://localhost:8081", "http://127.0.0.1:8081"],
     credentials: true,
   })
 );
