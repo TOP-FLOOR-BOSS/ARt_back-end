f870c6df2a5921007b31b0b111fb5357fb8b06af
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-09-06 11:43:52 +0200

    Admin Update

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(
     credentials: true,
   })
 );
-// app.use((req, res, next)=> {
-//   res.append('Access-Control-Allow-Origin', ['*']);
-//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.append('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// })
+app.use((req, res, next)=> {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 app.use(
   express.static("public"),
   router,
